06a18b3f9920934e85dcd86736567a6b48b98cd6
Maurice Flöthmann <48244051+Mau2rice0@users.noreply.github.com>
2025-06-12 11:23:18 +0200

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -1,10 +1,19 @@
 $ComputerName = "srv03-maurice.maurice.local"
 
 $ScriptBlock = {
-    Get-Process |
-        Sort-Object CPU -Descending |
-        Select-Object -First 5 Name, CPU, Id
+    Get-Process | ForEach-Object {
+        $proc = $_
+        Get-Service | Where-Object { $_.Name -eq $proc.Name } | ForEach-Object {
+            [PSCustomObject]@{
+                ServiceName = $_.DisplayName
+                Status      = $_.Status
+                CPU         = $proc.CPU
+                ProcessId   = $proc.Id
+            }
+        }
+    } | Sort-Object -Property CPU -Descending | Select-Object -First 5 ServiceName, Status, CPU, ProcessId
 }
 
 Invoke-Command -ComputerName $ComputerName -ScriptBlock $ScriptBlock
+
 
